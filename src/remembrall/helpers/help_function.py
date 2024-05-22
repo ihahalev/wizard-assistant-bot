da740b4354ba7a094f00bcb7f7a2abd1c11d2143
ArtemMokhonko <artem.stayl@gmail.com>
2024-05-22 20:57:09 +0300

    added help function

---
+++ src/remembrall/helpers/help_function.py
@@ -0,0 +1,11 @@
+from constants import commands
+from colorama import Fore, Style
+def show_help():
+    help_string = ''
+
+    for i in commands:
+        help_string += f' 💡 {Fore.CYAN}{i[0]:<16} | {i[1]:<30} | {i[2]:<12}{Style.RESET_ALL} \n'
+        
+    print(help_string)
+
+show_help()
